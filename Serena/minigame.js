9e50a589401d29174075b3eb25bf97099d8f4b63
Minh Khoi Ly <hugokhoiminhly@gmail.com>
2026-01-14 14:13:34 -0600

    Working on minigame, not finished

--- Serena/minigame.js
+++ Serena/minigame.js
@@ -5,3 +5,24 @@ const thirdChoice = document.getElementById("a3");
 const fourthChocie = document.getElementById("a4");
 const answer = document.getElementById("answer");
 const reset = document.getElementById("reset");
+const question = document.getElementById("question");
+const header = document.getElementById("qHeader");
+
+let counter = 0;
+const quesBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+const aBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+const bBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+const cBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+const dBank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+
+document.getElementById("mulChoice").addEventListener("submit", function(e){
+    e.preventDefault();
+    const selected = document.querySelector('input[name="q1"]:checked');
+
+    if(!selected) {
+        alert("Please select an answer!");
+        return;
+    }
+
+    console.log("Selected answer:", selected.value);
+}) 
---
+++ minigame.js
@@ -0,0 +1,7 @@
+const subBt = document.getElementById("submit");
+const firstChoice = document.getElementById("a1");
+const secondChoice = document.getElementById("a2");
+const thirdChoice = document.getElementById("a3");
+const fourthChocie = document.getElementById("a4");
+const answer = document.getElementById("answer");
+const reset = document.getElementById("reset");
